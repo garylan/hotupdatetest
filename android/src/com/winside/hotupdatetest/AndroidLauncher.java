ae6cc7bdfe16bd3a9a985c67b4912c653f4d5aad
garylan <garylan@126.com>
2017-07-24 17:26:38 +0800

    Create AndroidLauncher.java

---
+++ android/src/com/winside/hotupdatetest/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.winside.hotupdatetest;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.winside.hotupdatetest.MainGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new MainGame(new HotUpdateAndroid(getContext())), config);
+	}
+}
